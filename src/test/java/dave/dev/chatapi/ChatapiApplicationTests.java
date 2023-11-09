b01d8b2e6ae6e4446d45f0e7c2530526ae287795
John David Dela Cruz <johndaviddc1@gmail.com>
2023-11-09 16:50:27 +0800

    chat api

---
+++ src/test/java/dave/dev/chatapi/ChatapiApplicationTests.java
@@ -0,0 +1,13 @@
+package dave.dev.chatapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
