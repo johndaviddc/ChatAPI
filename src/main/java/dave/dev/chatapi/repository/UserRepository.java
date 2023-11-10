920d82c7fbf3ad3c2694771da2a3638be17fbf46
John David Dela Cruz <johndaviddc1@gmail.com>
2023-11-10 14:13:56 +0800

    added user repository

---
+++ src/main/java/dave/dev/chatapi/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package dave.dev.chatapi.repository;
+
+import dave.dev.chatapi.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>  {
+}
