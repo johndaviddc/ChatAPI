92692454e6dfd6d8101efc5adf9e861bf74648fa
John David Dela Cruz <johndaviddc1@gmail.com>
2023-11-13 14:27:47 +0800

    added service package

---
+++ src/main/java/dave/dev/chatapi/service/UserService.java
@@ -0,0 +1,17 @@
+package dave.dev.chatapi.service;
+
+import dave.dev.chatapi.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    
+}
