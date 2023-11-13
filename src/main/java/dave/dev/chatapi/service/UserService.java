2c5da67449dee30e960c12c5678a59fd16ec62cc
John David Dela Cruz <johndaviddc1@gmail.com>
2023-11-13 14:29:16 +0800

    added create user

--- src/main/java/dave/dev/chatapi/service/UserService.java
+++ src/main/java/dave/dev/chatapi/service/UserService.java
@@ -1,5 +1,6 @@
 package dave.dev.chatapi.service;
 
+import dave.dev.chatapi.model.User;
 import dave.dev.chatapi.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,6 +12,10 @@ public class UserService {
     @Autowired
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
     }
 
     
