f52e3248837bcebd38937f6bf10e9c4386aa54b6
John David Dela Cruz <johndaviddc1@gmail.com>
2023-11-16 20:22:21 +0800

    added controller package

---
+++ src/main/java/dave/dev/chatapi/controller/UserController.java
@@ -0,0 +1,27 @@
+package dave.dev.chatapi.controller;
+
+import dave.dev.chatapi.model.User;
+import dave.dev.chatapi.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User> registerUser(@RequestBody User user) {
+        User createdUser = userService.createUser(user);
+        return ResponseEntity.ok(createdUser);
+    }
+}
