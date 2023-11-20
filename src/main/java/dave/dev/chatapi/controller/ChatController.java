e4e4219acf5b7254411e653b6db048a9ffe94d6b
John David Dela Cruz <johndaviddc1@gmail.com>
2023-11-20 10:11:50 +0800

    added post mapping

--- src/main/java/dave/dev/chatapi/controller/ChatController.java
+++ src/main/java/dave/dev/chatapi/controller/ChatController.java
@@ -1,4 +1,31 @@
 package dave.dev.chatapi.controller;
 
+import dave.dev.chatapi.model.Message;
+import dave.dev.chatapi.service.MessageService;
+import dave.dev.chatapi.service.WebSocketService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/chat")
 public class ChatController {
+    private final MessageService messageService;
+    private final WebSocketService webSocketService;
+
+    @Autowired
+    public ChatController(MessageService messageService, WebSocketService webSocketService) {
+        this.messageService = messageService;
+        this.webSocketService = webSocketService;
+    }
+
+    @PostMapping("/send")
+    public ResponseEntity<Message> sendMessage(@RequestBody Message message) {
+        Message sentMessage = messageService.saveMessage(message);
+        webSocketService.sendMessage(sentMessage);
+        return ResponseEntity.ok(sentMessage);
+    }
 }
